604fabd80e421d613382eeeab14d2d5a74ca9559
Juan Andres Luna <108500388+andres1505@users.noreply.github.com>
2025-03-08 18:36:41 -0500

    Add files via upload

---
+++ miAlbum/apps/fotos/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Mifoto 
+# Register your models here.
+
+
+admin.site.register(Mifoto)
---
+++ miAlbum/apps/fotos/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Mifoto(models.Model):
+    nombre = models.CharField(max_length=90)
+    descripcion = models.CharField( max_length=2000 )
+    imageurl = models.CharField(max_length=120)
+    publish = models.BooleanField(default=True)
+    date = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.nombre
